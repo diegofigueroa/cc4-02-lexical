01dbbd193852bc4ad7cc71c3dd873b679976db8f
Diego Figueroa <diegofigueroa99@gmail.com>
2013-06-10 21:15:22 -0600

    added some UG assets.

--- slide_config.js
+++ slide_config.js
@@ -2,14 +2,14 @@ var SLIDE_CONFIG = {
   // Slide settings
   settings: {
     title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
+    subtitle: 'Ciencias de la Computaci&oacute;n IV',
     //eventTitle: 'Google I/O 2013',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/ug_24.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
@@ -19,12 +19,12 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'Diego Figueroa',
+    company: 'Universidad Galileo',
+    gplus: '+diegofigueroa',
+    twitter: '@diegofigueroa',
+    www: '',
+    github: 'http://github.com/diegofigueroa'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
