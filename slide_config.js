76f73318d8f57b6423676a05b2797973c0474ca0
Diego Figueroa <diegofigueroa99@gmail.com>
2013-06-22 18:56:04 -0600

    doing some cleanup.

--- slide_config.js
+++ slide_config.js
@@ -3,12 +3,11 @@ var SLIDE_CONFIG = {
   settings: {
     title: 'Title Goes Here<br>Up To Two Lines',
     subtitle: 'Ciencias de la Computaci&oacute;n IV',
-    //eventTitle: 'Google I/O 2013',
+    eventTitle: '2013',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
-    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
     favIcon: 'images/ug_24.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
@@ -16,22 +15,12 @@ var SLIDE_CONFIG = {
     ],
     //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
   },
-
+  
   // Author information
   presenters: [{
     name: 'Diego Figueroa',
     company: 'Universidad Galileo',
-    gplus: '+diegofigueroa',
-    twitter: '@diegofigueroa',
-    www: '',
-    github: 'http://github.com/diegofigueroa'
-  }/*, {
-    name: 'Second Name',
-    company: 'Job Title, Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
-  }*/]
+    email: 'diegofigueroa@galileo.edu',
+  }]
 };
 
